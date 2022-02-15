ad2f2e9e64888ab9850fe03cae7bf6c7baff894f
Nardo Kraaijeveld <nardo.kra@gmail.com>
2022-02-15 23:24:11 +0100

    Data fetching. Now with authorization via GraphCMS content token. TODO: Add test framework.

--- src/utils/api/cmsClient.ts
+++ src/utils/api/cmsClient.ts
@@ -1,6 +1,10 @@
 import { GraphQLClient, gql } from "graphql-request";
 
-const client = new GraphQLClient(process.env.NEXT_PUBLIC_GRAPHCMS_URL || "");
+const client = new GraphQLClient(process.env.NEXT_PUBLIC_GRAPHCMS_URL || "", {
+  headers: {
+    Authorization: `Bearer ${process.env.GRAPHCMS_CONTENT_TOKEN}`,
+  },
+});
 
 export const cmsClient = async <T>(query: string, vars?: {}): Promise<T> =>
   await client.request(
