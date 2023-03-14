facdcad4491c7d3cca8d55c8bf8e1aa1dba33912
Nardo Kraaijeveld <nardo.kra@gmail.com>
2023-03-14 15:40:20 +0100

    Resolved one small styling where the logo was falling out of the layout on safari mobile.

--- src/components/molecules/logoDisplay/LogoDisplay.tsx
+++ src/components/molecules/logoDisplay/LogoDisplay.tsx
@@ -7,7 +7,7 @@ type LogoDisplayType = { children: ReactNode };
 
 export const LogoDisplay = ({ children }: LogoDisplayType) => {
   const wrapperClasses = cx(
-    "flex items-center justify-center",
+    "flex justify-center",
     "w-full h-[8rem] p-2 border-2",
     "bg-stone-800 rounded-lg"
   );
